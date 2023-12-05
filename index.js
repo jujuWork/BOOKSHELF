8ceedc082633dfc944760097f78fbffbb784c800
jujuWork <joharrywork@gmail.com>
2023-12-05 22:19:02 +0900

    dec5

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-function myFunction () {
-    let x = document.getElementById("myTopnav");
-    if (x.className === "nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav";
+//imageslider function
+
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    let i;
+    let x = document.getElementsByClassName("featureslide");
+        if (n > x.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = x.length} ;
+            for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
     }
+        x[slideIndex-1].style.display = "block";
 }
